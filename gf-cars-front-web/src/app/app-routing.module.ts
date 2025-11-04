579f2bf726d0fbe9f4edc7609e7303a347cb668e
facm404 <facm.404@gmail.com>
2025-11-04 16:02:16 -0300

    diseño de navbar y banner listo

--- gf-cars-front-web/src/app/app-routing.module.ts
+++ gf-cars-front-web/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainHomeComponent } from './pages/main/home/main-home/main-home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+  path: 'home',component: MainHomeComponent
+}
+,
+{
+  path: '',
+  loadChildren: () => import('./pages/main/main-routing.module').then(m => m.MainRoutingModule)
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
